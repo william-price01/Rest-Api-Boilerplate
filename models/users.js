f46cc0ecb41e476bf87ff6c414a1d81698229e53
William Price <willvrd88@gmail.com>
2022-02-04 10:59:23 -0800

    First Commit

---
+++ models/users.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  users.init({
+    Id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Email: DataTypes.STRING,
+    Username: DataTypes.STRING,
+    Password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  return users;
+};
